89900ce7fa069a2029c7566061d174da0d3ea001
Tyler Larson <2002tjlarson2@gmail.com>
2025-10-22 23:04:00 -0500

    feat: improve file structure, add lazy loading github stats

---
+++ src/components/home/github-stats/index.ts
@@ -0,0 +1,9 @@
+export { ContributionDetailDialog } from "./contribution-detail-dialog";
+export { ContributionDialogManager } from "./contribution-dialog-manager";
+export { ContributionGraph } from "./contribution-graph";
+export { ContributionStats } from "./contribution-stats";
+export { GitHubStats } from "./github-stats";
+export { GitHubStatsLazy } from "./github-stats-lazy";
+export { useGitHubStats, useGitHubContributions, fetchContributionDetails } from "./queries";
+export type { ContributionDetail } from "./queries";
+
