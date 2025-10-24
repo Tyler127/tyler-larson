16d6222a6ce04d87130691625ad05ce75e8cbd65
Tyler Larson <2002tjlarson2@gmail.com>
2025-10-23 22:27:35 -0500

    feat: use new github activity component on GitHub page

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { GitHubStats } from "@/components/home/github-stats";
+import { GitHubActivityDashboard } from "@/components/github-activity";
 
 export default function GitHubPage() {
   return (
     <div className="min-h-screen py-12">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <GitHubStats />
+        <GitHubActivityDashboard username="Tyler127" />
       </div>
     </div>
   );
