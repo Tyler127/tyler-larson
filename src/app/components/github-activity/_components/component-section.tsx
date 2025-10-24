076faede35327a69a341962bad332f9895ca70c1
Tyler Larson <2002tjlarson2@gmail.com>
2025-10-23 23:01:21 -0500

    chore: remove old component preview components

--- src/app/components/github-activity/_components/component-section.tsx
+++
@@ -1,9 +0,0 @@
-export function CodeBlock({ code }: { code: string }) {
-  return (
-    <div className="relative">
-      <pre className="p-4 rounded-lg bg-muted overflow-x-auto">
-        <code className="text-sm">{code}</code>
-      </pre>
-    </div>
-  );
-}
